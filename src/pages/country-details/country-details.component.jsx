29ae8ce1c575ae2da5a1ba39515480cea7fada03
drood87 <dbernstein1987@hotmail.com>
2019-08-14 16:17:22 +0800

    border conutries redirect now to new country

--- src/pages/country-details/country-details.component.jsx
+++ src/pages/country-details/country-details.component.jsx
@@ -11,21 +11,34 @@ import CountryDetail from '../../components/country-details-body/country-details
 import './country-details.styles.scss';
 
 class CountryDetails extends Component {
-  state = {
-    countryNames: [],
-    country: [],
-  };
+  constructor(props) {
+    super(props);
 
-  componentDidMount() {
+    const {
+      location: { state },
+    } = this.props;
+
     const {
       location: {
         state: { borderNamesCode, borders },
       },
     } = this.props;
 
+    this.state = {
+      details: state,
+      countryNamesFromBorders: [],
+      borderNamesCode,
+      borders,
+      country: [],
+    };
+  }
+
+  componentDidMount() {
     /* ----------------------------------------------------------------------------*/
     // create new array that checks alpha codes against country names and then spits // out full names instead of the alpha3code
     /* ----------------------------------------------------------------------------*/
+    // state from React Router which is passed in as props and receive it from location
+    const { borderNamesCode, borders } = this.state;
 
     const fullBorderNames = (function codeToName() {
       const names = borders.map(borderCountry =>
@@ -36,18 +49,16 @@ class CountryDetails extends Component {
       return names.flat();
     })();
 
-    /* ------------------------------------------*/
-    // retrieve countries object from local storage
-    // !!!!dunno how to use that yet!!!!
-    /* ------------------------------------------*/
-
-    const countries = localStorage.getItem('countries');
-
     /* ----------------------------------------------------------*/
     // filter out countries which are not a border to that country
     /* ----------------------------------------------------------*/
 
     const filterCountries = (function filter() {
+      /* ------------------------------------------*/
+      // retrieve countries object from local storage
+      /* ------------------------------------------*/
+
+      const countries = localStorage.getItem('countries');
       const retrieveCountries = JSON.parse(countries);
 
       const filterBorders = borders.map(borderCountry =>
@@ -59,29 +70,65 @@ class CountryDetails extends Component {
       return filterBorders.flat();
     })();
 
-    const {
-      location: { state },
-    } = this.props;
+    // change state based on button click and replace with details of that country
 
     this.setState({
-      countryNames: fullBorderNames,
+      countryNamesFromBorders: fullBorderNames,
       country: filterCountries,
-      details: state,
     });
   }
 
-  // change state based on button click and replace with details of that country
+  // handle click on border country
 
-  handleBorder = event => {
-    console.log(event.target);
+  handleBorder = border => {
+    const { country, borderNamesCode } = this.state;
+
+    const borderCountryClicked = country.find(
+      countryBorder => countryBorder.name === border
+    );
+
+    // get borders from new country and transform into full Names from alpha3Code
+
+    const newBorders = borderCountryClicked.borders;
+
+    const createFullNames = (function newBordersToName() {
+      const names = newBorders.map(borderCountry =>
+        borderNamesCode
+          .filter(borderName => borderName.code === borderCountry)
+          .map(countryObj => countryObj.name)
+          .flat()
+      );
+      return names.flat();
+    })();
+
+    const filterCountries = (function filter() {
+      /* ------------------------------------------*/
+      // retrieve countries object from local storage
+      /* ------------------------------------------*/
+
+      const countries = localStorage.getItem('countries');
+      const retrieveCountries = JSON.parse(countries);
+
+      const filterBorders = newBorders.map(borderCountry =>
+        retrieveCountries
+          .filter(countryName => countryName.alpha3Code === borderCountry)
+          .map(countryObj => countryObj)
+      );
+
+      return filterBorders.flat();
+    })();
+
+    // update state with new country --> still need to update the borders with function from componentDidMount
+
+    this.setState(prevState => ({
+      details: borderCountryClicked,
+      countryNamesFromBorders: createFullNames,
+      country: filterCountries,
+    }));
   };
 
   render() {
-    const {
-      location: { state },
-    } = this.props;
-
-    const { countryNames, country } = this.state;
+    const { countryNamesFromBorders, details } = this.state;
 
     return (
       <div className="country-details">
@@ -93,31 +140,31 @@ class CountryDetails extends Component {
         </Link>
         <div className="country-details__body">
           <div className="country-details__flag">
-            <img src={state.flag} alt={`${state.name} flag`} />
+            <img src={details.flag} alt={`${details.name} flag`} />
           </div>
           <div className="country-details__information">
-            <h1 className="title">{state.name}</h1>
+            <h1 className="title">{details.name}</h1>
             <div className="country-details__main">
               <div>
-                <CountryDetail detail={state.nativeName}>
+                <CountryDetail detail={details.nativeName}>
                   Native Name:
                 </CountryDetail>
-                <CountryDetail detail={state.population.toLocaleString()}>
+                <CountryDetail detail={details.population.toLocaleString()}>
                   Population:
                 </CountryDetail>
-                <CountryDetail detail={state.region}>Region:</CountryDetail>
-                <CountryDetail detail={state.subregion}>
+                <CountryDetail detail={details.region}>Region:</CountryDetail>
+                <CountryDetail detail={details.subregion}>
                   Sub Region:
                 </CountryDetail>
-                <CountryDetail detail={state.capital}>Capital:</CountryDetail>
+                <CountryDetail detail={details.capital}>Capital:</CountryDetail>
               </div>
               <div>
-                <CountryDetail detail={state.topLevelDomain[0]}>
+                <CountryDetail detail={details.topLevelDomain[0]}>
                   Top Level Domain:
                 </CountryDetail>
                 <CountryDetail
-                  detail={state.currencies.map((currency, idx) =>
-                    idx < state.currencies.length - 1
+                  detail={details.currencies.map((currency, idx) =>
+                    idx < details.currencies.length - 1
                       ? `${currency.name}, `
                       : currency.name
                   )}
@@ -125,8 +172,8 @@ class CountryDetails extends Component {
                   Currencies:
                 </CountryDetail>
                 <CountryDetail
-                  detail={state.languages.map((language, idx) =>
-                    idx < state.languages.length - 1
+                  detail={details.languages.map((language, idx) =>
+                    idx < details.languages.length - 1
                       ? `${language.name}, `
                       : language.name
                   )}
@@ -138,15 +185,17 @@ class CountryDetails extends Component {
             <div className="country-details__borders">
               <p>Border Countries:</p>
               <div className="country-details__button-wrapper">
-                {countryNames.map(border => (
-                  <CustomButton
-                    key={border}
-                    onClick={this.handleBorder}
-                    isCardDetails
-                    isDarkMode={false}
-                  >
-                    {border}
-                  </CustomButton>
+                {countryNamesFromBorders.map(border => (
+                  <Link to={`${border}`}>
+                    <CustomButton
+                      onClick={() => this.handleBorder(border)}
+                      key={border}
+                      isCardDetails
+                      isDarkMode={false}
+                    >
+                      {border}
+                    </CustomButton>
+                  </Link>
                 ))}
               </div>
             </div>
@@ -159,7 +208,7 @@ class CountryDetails extends Component {
 
 CountryDetails.propTypes = {
   location: PropTypes.object,
-  state: PropTypes.object,
+  details: PropTypes.object,
 };
 
 export default withRouter(CountryDetails);
